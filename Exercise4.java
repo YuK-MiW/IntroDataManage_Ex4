897c139d41d32c5c50614c85435645321d6e69d6
Raverill <raverill@dummy.mail>
2019-07-10 16:06:34 +0900

    Judge a game Revise, also, add total variable

--- Exercise4.java
+++ Exercise4.java
@@ -4,6 +4,7 @@ class Exercise4{
   public static void main(String[] args){
     int dice1;
     int dice2;
+    int total;
     Random r;
 
     r = new Random();
@@ -11,10 +12,15 @@ class Exercise4{
     // r.nextInt(6) generates 0 to 5
     dice1 = r.nextInt(6) + 1;
     dice2 = r.nextInt(6) + 1;
+    total = dice1 + dice2;
+
 
     System.out.println("Rolling the dice...");
     System.out.println("Dic 1: " + dice1);
     System.out.println("Dic 2: " + dice2);
-    System.out.println("Total value: " + (dice1 + dice2));
+    System.out.println("Total value: " + total);
+
+    if(total >= 7)System.out.println("You won!");
+    else System.out.println("You lost!");
   }
 }
